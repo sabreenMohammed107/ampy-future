eaf488b79bb930007e66ab1af8913334a818f40e
sabreenMohammed107 <sabreenm312@gmail.com>
2022-03-30 00:46:59 +0200

    img

--- app/Http/Resources/UserDataResource.php
+++ app/Http/Resources/UserDataResource.php
@@ -20,7 +20,7 @@ class UserDataResource extends JsonResource
             "email" => $this->email ?? '',
             "mobile" => $this->mobile,
             "emp_code" => $this->emp_code,
-            "image" => $this->profile ? asset('uploads/users/' . $this->image) : env('APP_URL') . '/storage/default_profile.jpeg',
+            "image" => $this->image ? asset('uploads/users/' . $this->image) : env('APP_URL') . '/storage/default_profile.jpeg',
             "register_approved" => $this->register_approved ?? 0,
             'transaction' => TransactionResource::collection($this->transation->take(3)),
             "latestTransaction" => TransactionResource::make($this->latestTransation),
