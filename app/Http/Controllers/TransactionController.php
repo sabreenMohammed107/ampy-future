6ee27abbbec6fc4149c4dbaee0414b851840da83
sabreenMohammed107 <sabreenm312@gmail.com>
2022-05-10 22:11:40 +0200

    rr

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -237,21 +237,17 @@ class TransactionController extends Controller
                         'status' => 'not_seen',
                     ];
 
-                    // FCMService::send($trans->user->fcm_token, $data,
-                    //     [
-                    //         'message' => 'Extra Notification Data',
-                    //     ],
-                    // );
-                    // FCMService::send(
-                    //     $trans->user->fcm_token,
-                    //     [
-                    //         'title' => 'your title',
-                    //         'body' => 'your body',
-                    //     ],
-                    //     [
-                    //       'message' => 'Extra Notification Data'
-                    //     ],
-                    // );
+
+                    FCMService::send(
+                        $trans->user->fcm_token,
+                        [
+                            'title' => 'your title',
+                            'body' => 'your body',
+                        ],
+                        [
+                          'message' => 'Extra Notification Data'
+                        ],
+                    );
 
 
 
@@ -273,34 +269,7 @@ class TransactionController extends Controller
 
 
              //test sabreen
-             $firebaseToken = User::whereNotNull('fcm_token')->pluck('fcm_token')->all();
 
-             $SERVER_API_KEY = 'AAAA3TCDdrE:APA91bHborGVe-kYXv2ILUlYmCJj9_6g8dz08QidlYQc9i_xGCUUo0IDRoxaiLRyWVrgvfv3J3GwYBJe2ietTenT5IQBf7619j29fHDNzzdXK22jzXSVSkcT09vf0U4yBbL4afNRLZDH';
-
-             $data = [
-                 "registration_ids" => $firebaseToken,
-                 "notification" => [
-                     "title" => $request->title,
-                     "body" => $request->body,
-                 ]
-             ];
-             $dataString = json_encode($data);
-
-             $headers = [
-                 'Authorization: key=' . $SERVER_API_KEY,
-                 'Content-Type: application/json',
-             ];
-
-             $ch = curl_init();
-
-             curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
-             curl_setopt($ch, CURLOPT_POST, true);
-             curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-             curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString);
-
-             $response = curl_exec($ch);
              //end test
 
             // Display a successful message ...
